f46dbfad148859e0e6392160570c1f453e3d566b
Kamil Khuzzyatov <kkhuzzyatov@gmail.com>
2026-08-18 12:20:30 +0300

    feat: add useful methods to map

--- src/main/kotlin/simulation/general/Map.kt
+++ src/main/kotlin/simulation/general/Map.kt
@@ -1,17 +1,26 @@
 package simulation.general
 
-class Map (
-    private val map: MutableMap<Coordinates, Entity> = mutableMapOf(),
+class Map(
+    private val cells: MutableMap<Coordinates, Entity> = mutableMapOf(),
 ) {
-    fun get(coordinate: Coordinates): Entity? {
-        return map[coordinate]
+
+    fun get(coordinate: Coordinates): Entity? =
+        cells[coordinate]
+
+    fun isEmpty(coordinate: Coordinates): Boolean =
+        !cells.containsKey(coordinate)
+
+    fun remove(coordinate: Coordinates) {
+        cells.remove(coordinate)
     }
 
-    fun clear(coordinate: Coordinates) {
-        map.remove(coordinate)
+    fun put(coordinate: Coordinates, entity: Entity) {
+        cells[coordinate] = entity
     }
 
-    fun update(coordinate: Coordinates, entity: Entity) {
-        map[coordinate] = entity
+    fun move(from: Coordinates, to: Coordinates) {
+        val entity = cells[from] ?: return
+        cells.remove(from)
+        cells[to] = entity
     }
 }
