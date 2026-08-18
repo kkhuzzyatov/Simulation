4532a17f6ce9a3217ff315ca6d4e5dee9d29e980
Kamil Khuzzyatov <kkhuzzyatov@gmail.com>
2026-08-18 18:16:27 +0300

    chore: rename images set to ImageConfig

---
+++ src/main/kotlin/simulation/config/ImageConfig.kt
@@ -0,0 +1,9 @@
+package simulation.config
+
+object ImageConfig {
+    const val PREDATOR = "🐅"
+    const val HERBIVORE = "🐇"
+    const val GRASS = "🌿"
+    const val ROCK = "🪨"
+    const val TREE = "🌳"
+}
--- src/main/kotlin/simulation/config/Images.kt
+++
@@ -1,9 +0,0 @@
-package simulation.config
-
-object Signs {
-    val PREDATOR = "🐅"
-    val HERBIVORE = "🐇"
-    val GRASS = "🌿"
-    val ROCK = "🪨"
-    val TREE = "🌳"
-}
